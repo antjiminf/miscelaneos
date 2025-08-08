f61d59484f19de630c804b47eb0a9ebe0a0c481d
Your Name <antoniojz1998@gmail.com>
2025-08-08 13:59:05 +0200

    Utilities providers implemented

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,1 +1,6 @@
 export 'app_state_provider.dart';
+export 'permissions/perrmissions_provider.dart';
+export 'sensors/gyroscope_provider.dart';
+export 'sensors/accelerometer_providers.dart';
+export 'sensors/magnetometer_provider.dart';
+export 'compass/compass_provider.dart';
